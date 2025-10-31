616cddd327fd1aecfc20cd32353f7ac30a6cc36b
h_r_m <pitechworks@gmail.com>
2025-10-30 18:00:32 -0700

    day and night theme

--- planetravel/tailwind.config.js
+++ planetravel/tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: ['light', 'dark'],
+    darkTheme: 'dark',
   },
-}
+};
