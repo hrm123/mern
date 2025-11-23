8aca8347533e1a04ac8d6abcad1d587047081e5e
h_r_m <pitechworks@gmail.com>
2025-11-23 08:33:41 -0800

    tests added

--- planetravel/vite.config.ts
+++ planetravel/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,9 @@ export default defineConfig({
     port: 8888,
     strictPort: true,
   },
-})
+  test: {
+    environment: 'jsdom', // Use 'jsdom' for browser environments, 'node' for Node.js
+    globals: true, // Allows using `expect`, `describe`, `it` globally without imports
+    setupFiles: './test/setup.ts', // Optional: Path to a setup file for global configurations
+  },
+});
